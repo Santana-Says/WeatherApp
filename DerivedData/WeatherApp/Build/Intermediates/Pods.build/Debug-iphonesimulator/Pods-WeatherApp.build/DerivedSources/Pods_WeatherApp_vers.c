511eefd45030e776c6951058603b852b9e08cff1
Jeffrey Santana <Santana@jeffreys-mini.attlocal.net>
2017-09-29 11:10:09 -0400

    Simple layout, added Alamofire to project

---
+++ DerivedData/WeatherApp/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-WeatherApp.build/DerivedSources/Pods_WeatherApp_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char Pods_WeatherAppVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_WeatherApp  PROJECT:Pods-1" "\n"; const double Pods_WeatherAppVersionNumber __attribute__ ((used)) = (double)1.;
